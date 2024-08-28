41a20129ab124d5f861b487563e18620849e5b9a
crazybot123 <day1228@livemail.tw>
2024-08-28 08:03:03 +0800

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToAddRows = false;
+            this.dgvData.AllowUserToDeleteRows = false;
+            this.dgvData.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvData.Location = new System.Drawing.Point(0, 0);
+            this.dgvData.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.ReadOnly = true;
+            this.dgvData.RowHeadersWidth = 62;
+            this.dgvData.RowTemplate.Height = 24;
+            this.dgvData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvData.Size = new System.Drawing.Size(1503, 880);
+            this.dgvData.TabIndex = 93;
+            this.dgvData.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvData_CellClick);
+            this.dgvData.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvData_CellContentClick);
+            this.dgvData.Paint += new System.Windows.Forms.PaintEventHandler(this.dgvData_Paint);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(471, 89);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(109, 22);
+            this.checkBox1.TabIndex = 94;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1503, 880);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dgvData);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        BindingSource customersBindingSource = new BindingSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Add columns to the DataGridView
+            dgvData.Columns.Add("Id", "Id");
+            dgvData.Columns.Add("Name", "Name");
+
+            // add button in datagridview c#
+            // Add a button column
+            DataGridViewButtonColumn buttonColumn = new DataGridViewButtonColumn();
+            buttonColumn.HeaderText = "Action";
+            buttonColumn.Text = "Delete";
+            buttonColumn.UseColumnTextForButtonValue = true; // Set to true to use Text property for the button
+            dgvData.Columns.Add(buttonColumn);
+
+            // Sample data
+            dgvData.Rows.Add(1, "John");
+            dgvData.Rows.Add(2, "Alice");
+            dgvData.Rows.Add(3, "Bob");
+        }
+
+        private void dgvData_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Check deleted rows
+            if (dgvData.Columns[e.ColumnIndex].Name == "Delete")
+            {
+                if (MessageBox.Show("Are you sure want to delete this record ?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    customersBindingSource.RemoveCurrent();
+            }
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Subscribe to the CellClick event
+            dgvData.CellClick += dgvData_CellClick;
+            //Init data
+            customersBindingSource.Add(new Customers() { CustomerID = "1", CustomerName = "Maria Anders", Email = "maria@gmail.com", Address = "Obere Str. 57" });
+            customersBindingSource.Add(new Customers() { CustomerID = "2", CustomerName = "Ana Trujillo", Email = "ana171@gmail.com", Address = "Avada. de la Cons" });
+            customersBindingSource.Add(new Customers() { CustomerID = "3", CustomerName = "Thomas Hardy", Email = "thomad@gmail.com", Address = "120 Hanover Sq." });
+            customersBindingSource.Add(new Customers() { CustomerID = "4", CustomerName = "Elizabeth Liconln", Email = "elizabeth@gmail.com", Address = "23 Tsawassen" });
+        }
+
+        private void dgvData_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Check if the clicked cell is a button cell and its column index is the button column
+            if (e.ColumnIndex == dgvData.Columns["Delete"].Index && e.RowIndex >= 0)
+            {
+                // You can perform actions based on the row clicked
+                MessageBox.Show($"Button clicked in row {e.RowIndex}");
+            }
+        }
+
+        private void dgvData_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+
+    public class Customers
+    {
+        public string CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
+
